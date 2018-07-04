7e721e5c92d64c2b92db861473df59bde9e5ed19
Matt Mahin <matt.mahin@qsc.com>
2018-07-03 21:19:10 -0600

    * canned wakeup routine

--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
 pwd = $(shell pwd)
-alarm-clock: main.o
-	gcc -L$(pwd) -Wall -Werror -o alarm-clock main.o -lcolor-temp -lws2811 -lm
+alarm-light: main.o
+	gcc -L$(pwd) -Wall -Werror -o alarm-light main.o -lcolor-temp -lws2811 -lm
 main.o: main.c color_temp.h ws2811.h
 	gcc -c -Wall -Werror main.c
 .PHONY: clean
 clean:
-	rm -f alarm-clock main.o
+	rm -f alarm-light main.o
--- main.c
+++ main.c
@@ -5,14 +5,18 @@
 #include <signal.h>
 #include <unistd.h>
 #include <math.h>
+#include <time.h>
 
 #include "ws2811.h"
 #include "color_temp.h"
 
-#define WIDTH                   120
-#define HEIGHT                  1
-#define LED_COUNT               (WIDTH * HEIGHT)
-#define FPS                     16
+#define WIDTH       (120)
+#define HEIGHT      (1)
+#define LED_COUNT   (WIDTH * HEIGHT)
+#define FPS         (16)
+
+#define LOG2_255p5  (7.997179481)
+#define LOG2_511p5  (8.998590430)
 
 ws2811_t ledstring = {
   .freq = 800000,
@@ -57,45 +61,20 @@ setup_handlers(
   sigaction(SIGTERM, &sa, NULL);
 }
 
-uint32_t *color_table;
-
-static int
-color_table_init(
-		int size,
-    double kMin,
-    double kMax
-)
-{
-  if (color_table > 0)
-		free(color_table);
-	color_table = malloc(size * sizeof(color_table[0]));
-	if (color_table <= 0) {
-		fprintf(stderr, "failed to allocate color table\n");
-		return -1;
-	}
-
-	int ii;
-	double step = (kMax - kMin) / (size - 1);
-	color_temp_t ct;
-	for (ii=0; ii<size; ii++) {
-		ct.kelvin = step * ii + kMin;
-		interp_color_temp(&ct);
-		color_table[ii] = ((uint32_t)ct.byte_red) << 16;
-		color_table[ii] |= ((uint32_t)ct.byte_green) << 8;
-		color_table[ii] |= ((uint32_t)ct.byte_blue) << 0;
-    //printf("%04d kelvin: %08x\n", (int)nearbyint(ct.kelvin), color_table[ii]);
-	}
-
-	return 0;
-}
-
 int
 main(
     int argc,
     char *argv[]
 )
 {
+  // catch signals
   setup_handlers();
+
+  int ii;
+
+  // record startTime
+  struct timespec startTime, now;
+  clock_gettime(CLOCK_REALTIME, &startTime);
 
   ws2811_return_t ret;
   if ((ret = ws2811_init(&ledstring)) != WS2811_SUCCESS) {
@@ -103,20 +82,43 @@ main(
     return ret;
   }
 
-  const int table_size = 256;
-	if (color_table_init(table_size, 2500, 5500))
-		return -1;
+  // seed rand()
+  srand(time(0));
 
- 	int ii;
-  int shift = 0;
-  double dub_idx;
   while (running) {
-		for (ii=0; ii<LED_COUNT; ii++) {
-      dub_idx = (shift + ii) * table_size / (double)LED_COUNT;
-			ledstring.channel[0].leds[ii] = color_table[(int)nearbyint(dub_idx) % table_size];
+    // elapsed time
+    clock_gettime(CLOCK_REALTIME, &now);
+    double d_secSinceStart = (now.tv_sec - startTime.tv_sec) + 1e-9 * (now.tv_nsec - startTime.tv_nsec);
+
+    // brightness
+    double d_maxBright = 0 + 0.033 * d_secSinceStart;
+    d_maxBright = (d_maxBright < 0) ? 0 : (d_maxBright > LOG2_511p5) ? LOG2_511p5 : d_maxBright;
+    int minBright;
+    int maxBright;
+    if (d_maxBright >= LOG2_255p5) {
+      maxBright = 255;
+      minBright = (int)pow(2.0, LOG2_511p5 - d_maxBright);
+    } else {
+      maxBright = (int)pow(2.0, d_maxBright);
+      minBright = maxBright;
     }
-    ledstring.channel[0].brightness = 0x20;
-    shift = (shift + 1) % table_size;
+
+    // color temp
+    color_temp_t color;
+    color.kelvin = 1000 +  10.0 * d_secSinceStart;
+    interp_color_temp(&color);
+    //printf("r%0.3f g%0.3f b%0.3f w%0.3f\n", color.norm_red, color.norm_green, color.norm_blue, 0.0);
+
+    // synthesize colors & brightness
+    uint32_t red, green, blue, white;
+    for (ii=0; ii<LED_COUNT; ii++) {
+      white = (uint32_t)(255.0 * 0                * 0.5 * (1.0 + rand() / (double)RAND_MAX)) << 24;
+      red   = (uint32_t)(255.0 * color.norm_red   * 0.5 * (1.0 + rand() / (double)RAND_MAX)) << 16;
+      green = (uint32_t)(255.0 * color.norm_green * 0.5 * (1.0 + rand() / (double)RAND_MAX)) <<  8;
+      blue  = (uint32_t)(255.0 * color.norm_blue  * 0.5 * (1.0 + rand() / (double)RAND_MAX)) <<  0;
+      ledstring.channel[0].leds[ii] = white | red | green | blue;
+    }
+    ledstring.channel[0].brightness = (uint8_t) (minBright + ((maxBright - minBright) * (rand() / (double)RAND_MAX)));
 
     if ((ret = ws2811_render(&ledstring)) != WS2811_SUCCESS) {
       fprintf(stderr, "ws2811_render failed: %s\n", ws2811_get_return_t_str(ret));
@@ -126,6 +128,7 @@ main(
     usleep(1000000 / FPS);
   }
 
+  // turn off LEDs & get out
   for (ii=0; ii<LED_COUNT; ii++)
     ledstring.channel[0].leds[ii] = 0;
   ws2811_render(&ledstring);
